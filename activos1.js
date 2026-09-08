d382a457f3e764b8e81bbd9e0c7c5af11620ec07
mdchi2 <mdchmariano1@gmail.com>
2026-09-08 10:33:11 -0300

    Add files via upload

---
+++ activos1.js
@@ -0,0 +1,33 @@
+// Lista de símbolos para activos1.html
+// Puedes agregar, quitar o reordenar los símbolos aquí:
+const SYMBOLS_LIST = `
+BTC
+ETH
+USDT
+BNB
+XRP
+SOL
+HYPE
+ZEC
+XMR
+SUI
+AVAX
+TAO
+ONDO
+MNT
+AAVE
+RENDER
+KAS
+ARB
+NEXO
+PUMP
+AERO
+XPL
+AR
+XPIN
+RON
+LINEA
+MMT
+THE
+FOXY
+`;
---
+++ activos2.js
@@ -0,0 +1,22 @@
+// Lista de símbolos para activos1.html
+// Puedes agregar, quitar o reordenar los símbolos aquí:
+const SYMBOLS_LIST = `
+BNB
+XRP
+SOL
+HYPE
+XMR
+AVAX
+TAO
+ONDO
+MNT
+AAVE
+RENDER
+KAS
+ARB
+PUMP
+AERO
+XPL
+AR
+ASTER
+`;
